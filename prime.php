0b16885bdfb7c7feb6deb0474a1a1a8278bb9122
loiseupro <86427844+loiseupro@users.noreply.github.com>
2021-06-24 18:59:38 +0200

    Update prime.php

--- prime.php
+++ prime.php
@@ -3,13 +3,10 @@
 function isPrime($num){
 
    $cont = 0;
-   for ($i=1; $i <= $num; $i++) {
-      if ($num % $i==0) $cont = $cont + 1;
-   }
-
+   for ($i=1; $i <= $num; $i++)  if ($num % $i==0) $cont = $cont + 1; 
    if ($cont==2) return true;
-   return false;
 
+   return false;
 }
 
 ?>
